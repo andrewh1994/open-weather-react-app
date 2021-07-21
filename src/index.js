06e57807841c796561fffd3e112a3e8c8ffc25f7
andrewh1994 <andrewhallinan1994@gmail.com>
2021-07-21 01:02:35 +0100

    updates to clean up app

--- src/index.js
+++ src/index.js
@@ -1,138 +1,106 @@
+//jshint esversion:6
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import logo from './OpenWeather-Logo.jpg';
+require('dotenv').config();
 
+const apiKey = process.env.REACT_APP_API_KEY;       //takes REACT_APP_API_KEY from .env file
 
 //JSX is used to convert HTML into React elements
-const myweather_logo = 
-<div className="App">
+const myweatherLogo = <div className="App">
     <header className="App-header">
     <img src={logo} className="App-logo" alt="logo"  width="200"/>
     </header>
 </div> ;
+ReactDOM.render(myweatherLogo, document.getElementById('imageRoot'));
 
-ReactDOM.render(myweather_logo, document.getElementById('image_root'));
 
+var button = document.querySelector('#weatherButton');
+var textInput = document.querySelector('#textInput');
 
-var button = document.querySelector('.weather_button');
+button.addEventListener('click', start);
 
-button.addEventListener('click',  async function(){
+textInput.addEventListener('keypress' ,function(event){
+   // enter has keyCode = 13
+   if (event.keyCode === 13) {
+     start();
+   }
+ });
 
-    var inputValue = document.querySelector('.inputValue').value;
-    //console.log("Here is the inputValue "+ inputValue);
+async function start(){
+    var textInputValue = textInput.value;
 
-    
     const response = await fetch(
-        'http://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=32a2b57882deec5c590427c5e65b76ae'
+        'https://api.openweathermap.org/data/2.5/weather?q='+textInputValue+'&appid='+apiKey+'&units=metric'
     );
     const data = await response.json();
-    //console.log(data);
-    
-    var selected_city_name_value = data.name;
-    //console.log("Here is the selected_city_name_value: " + selected_city_name_value);
-    
-    var selected_city_temp_value_kelvin = data.main.temp;
-    var selected_city_temp_value_celsius = (selected_city_temp_value_kelvin - 273.15 ).toFixed(2);
-    //console.log("Here is the selected city temp: " + selected_city_temp_value_celsius);
 
-    var selected_city_feels_like_value_kelvin = data.main.feels_like;
-    var selected_city_feels_like_value_celsius = (selected_city_feels_like_value_kelvin - 273.15 ).toFixed(2);
-    //console.log("Here is what the selected city temp feels like: " + selected_city_feels_like_value_celsius);
+    var selectedCityName = data.name;
+    var selectedCityTemp = data.main.temp;
+    var selectedCityFeelsLike = data.main.feels_like;
+    var selectedCityDescription = data.weather[0].description
+    var iconCode = data.weather[0].icon;
+    var iconUrl = "https://openweathermap.org/img/w/"+iconCode+".png";
 
-    var selected_city_description = data.weather[0].description
-    //console.log("Here is the selected_city_description: " + selected_city_description);
+    const selectedCityWeatherImage = <img src={iconUrl} className="App-logo" alt="logo" width="30" height="30" />;
+    ReactDOM.render(selectedCityWeatherImage, document.getElementById('selectedCityWeatherImageRoot'));
 
-    var icon_code = data.weather[0].icon;
-    var icon_url = "http://openweathermap.org/img/w/" + icon_code + ".png";
-    //console.log("Here is the iconcode: " + icon_url);
+    var selectedCityInfo = 'Here is the temperature in ' +selectedCityName +': ' +selectedCityTemp +'°C  (feels like ' + selectedCityFeelsLike + '°C )' ;
+    ReactDOM.render(selectedCityInfo, document.getElementById('selectedCity'));
 
-    const selected_city_weather_image = 
-    <img src={icon_url} className="App-logo" alt="logo" width="30" height="30" />;
+    var selectedCityWeatherInfo = 'There is ' +selectedCityDescription +' in ' +selectedCityName +' currently.';
+    ReactDOM.render(selectedCityWeatherInfo, document.getElementById('selectedCityWeatherDescription'));
 
-    ReactDOM.render(selected_city_weather_image, document.getElementById('selected_city_weather_image_root'));
-
-    var selected_city_info = 'Here is the temperature in ' +selected_city_name_value +': ' +selected_city_temp_value_celsius +'°C  (feels like ' + selected_city_feels_like_value_celsius + '°C )' ;
-    ReactDOM.render(selected_city_info, document.getElementById('selected_city'));
-    
-    var selected_weather_description_info = 'There is ' +selected_city_description +' in ' +selected_city_name_value +' currently.';
-    ReactDOM.render(selected_weather_description_info, document.getElementById('selected_city_weather_description'));
-    
     get_user_city_data();
+}
 
-})
 
-
-function get_user_city_data() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+function get_user_city_data(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(set_user_coordinates);
     } else {
         console.error( "Error, geolocation is not supported by this browser.");
     }
 }
 
+function set_user_coordinates(position){
+    var userLatitude = position.coords.latitude;
+    var userLongitude = position.coords.longitude;
 
-function showPosition(position) {
-    
-    var user_latitude = position.coords.latitude;
-    var user_longitude = position.coords.longitude;
-    //console.log( "latitude is: " + user_latitude );
-    //console.log( "longitude is: " + user_longitude );
-
-    setLocation(user_latitude, user_longitude);
+    set_user_location(userLatitude, userLongitude);
 }
 
-
-async function setLocation(user_latitude, user_longitude){
-
-    const response = await fetch(
-        'http://api.openweathermap.org/data/2.5/weather?lat='+user_latitude+'&lon='+user_longitude+'&appid=32a2b57882deec5c590427c5e65b76ae'
-    ); 
-    const data = await response.json();
-    //console.log(data);
-
-    var user_city_name_value = data.name;
-    //console.log("Here is the user_city_name_value: " + user_city_name_value);
-
-    var user_city_temp_value_kelvin = data.main.temp;
-    var user_city_temp_value_celsius = (user_city_temp_value_kelvin - 273.15 ).toFixed(2);
-    //console.log("Here is the users city temp: " + user_city_temp_value_celsius);
-    
-    var user_city_info = 'Here is the temperature in your location (' +user_city_name_value + '): ' +user_city_temp_value_celsius +'°C';
-    
-    ReactDOM.render(user_city_info, document.getElementById('user_city'));
-
-    compare_selected_and_user_city_temp(user_city_temp_value_celsius, user_city_name_value);
-}  
-
-
-async function compare_selected_and_user_city_temp(user_city_temp_value_celsius, user_city_name_value){
-
-    var inputValue = document.querySelector('.inputValue').value;
-    //console.log("Here is the inputValue "+ inputValue);
+async function set_user_location(userLatitude, userLongitude){
 
     const response = await fetch(
-        'http://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=32a2b57882deec5c590427c5e65b76ae'
+        'https://api.openweathermap.org/data/2.5/weather?lat='+userLatitude+'&lon='+userLongitude+'&appid='+apiKey+'&units=metric'
     );
-    const data = await response.json();  
-    //console.log(data);
+    const data = await response.json();
 
-    var selected_city_name_value = data.name;
-    //console.log("Here is the selected_city_name_value: " + selected_city_name_value);
-    
-    var selected_city_temp_value_kelvin = data.main.temp;
-    var selected_city_temp_value_celsius = (selected_city_temp_value_kelvin - 273.15 ).toFixed(2);
-    //console.log("Here is the selected city temp for comparison: " + selected_city_temp_value_celsius);
-    //console.log("Here is the user city temp  for comparison: " + user_city_temp_value_celsius);
-    
-    var temperature_difference = (user_city_temp_value_celsius - selected_city_temp_value_celsius).toFixed(2);
-    //console.log("Here is the temp difference: " + temperature_difference);
-    
+    var userCityName = data.name;
+    var userCityTemp = data.main.temp;
+    var userCityInfo = 'Here is the temperature in your location (' +userCityName + '): ' +userCityTemp +'°C';
+    ReactDOM.render(userCityInfo, document.getElementById('userCity'));
 
-    var comparison_message = 'The difference in temperature between ' +user_city_name_value+ ' and '+selected_city_name_value 
-    + ' is: ' +temperature_difference +' °C';
-    
-    ReactDOM.render(comparison_message, document.getElementById('comparison'));
-    
+    compare_selected_and_user_city_temp(userCityTemp, userCityName);
 }
 
+async function compare_selected_and_user_city_temp(userCityTemp, userCityName){
+    var textInputValue = textInput.value;
+
+    const response = await fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q='+textInputValue+'&appid='+apiKey+'&units=metric'
+    );
+    const data = await response.json();
+
+    var selectedCityName = data.name;
+    var selectedCityTemp = data.main.temp;
+    var temperatureDifference = (userCityTemp - selectedCityTemp).toFixed(2);
+    var differenceDescriptor = ' colder than ';
+    if(temperatureDifference >= 0){
+      differenceDescriptor = ' warmer than ';
+    }
+
+    var comparisonMessage = userCityName+ ' is '+Math.abs(temperatureDifference)+ '°C' + differenceDescriptor +selectedCityName;
+    ReactDOM.render(comparisonMessage, document.getElementById('comparison'));
+}
